d41cbbd2a4fbbab52dd79f8b22d91cb5f07a6908
Shijia Wang <Shijia_Wang1@baylor.edu>
2022-02-21 15:45:11 -0600

    02212022

---
+++ CellPhone.py
@@ -0,0 +1,23 @@
+from pyexpat import model
+from tracemalloc import start
+import CellPhoneClass as cp
+
+
+def main():
+
+    manufact = input("Enter your cell phone manufact: ")
+
+    model = input("Enter your cell phone model: ")
+
+    retail_price = input("Enter your cell phone retail price: ")
+
+    cellphone = cp.CellPhone(manufact, model, retail_price)
+
+    print("The manufact is ", cellphone.get_manufact())
+
+    print("The mmodel is ", cellphone.get_model())
+
+    print("The retail price is ", cellphone.get_retail_price())
+
+
+main()
